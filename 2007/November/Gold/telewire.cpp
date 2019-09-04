5b2d60234c7275352044caf8da6a4ce71439cae2
Ta180m <ta180m@gmail.com>
2019-09-04 13:59:06 -0500

    Update telewire.cpp

--- 2007/November/Gold/telewire.cpp
+++ 2007/November/Gold/telewire.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 constexpr auto INF = (int)1e9;
 
-int N, C, H, A[105], B[105], DP[105];
-
 int main() {
+	int N, C, H, A[105], B[105], DP[105];
 	cin >> N >> C;
 	for (int i = 0; i < N; i++) { cin >> H;
 		for (int j = 0; j <= 100; j++) DP[j] = (j < H ? INF : (j - H) * (j - H) + (i > 0) * min(A[j] + C * j, B[j] - C * j));
